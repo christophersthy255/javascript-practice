263d790a08a7073a02dbe008324d2a41ee8d338a
christophersthy255 <59051166+christophersthy255@users.noreply.github.com>
2022-05-17 15:47:06 -0700

    Add files via upload

---
+++ javascript-practice/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('h1').onclick = changeH1
+
+function changeH1(){
+    document.querySelector('h1').innerText = 'Hi'
+    document.querySelector('h1').style.fontSize = '22rem'
+    document.querySelector('body').style.color = 'red'
+    document.querySelector('body').style.backgroundColor = 'Green'
+}
